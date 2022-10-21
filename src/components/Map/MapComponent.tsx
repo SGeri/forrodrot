0018e802c9f1c6b2b9dec251ae769f7f36c166ae
Erdei Gábor <erdei.gabor2002@gmail.com>
2022-10-21 20:45:44 +0200

    Map now only draggable with two fingers

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -1,25 +1,29 @@
 import { MapContainer, MapContainerProps, TileLayer } from "react-leaflet";
+import { useViewportSize } from "@mantine/hooks";
 import "leaflet/dist/leaflet.css";
 
 interface MapProps extends MapContainerProps {
-  height: number;
-  children: React.ReactNode;
+	height: number;
+	children: React.ReactNode;
 }
 
 const Map = ({ height, children, ...rest }: MapProps) => {
-  return (
-    <MapContainer
-      style={{ height, width: "100%" }}
-      scrollWheelZoom={false}
-      {...rest}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {children}
-    </MapContainer>
-  );
+	const { width } = useViewportSize();
+
+	return (
+		<MapContainer
+			style={{ height, width: "100%" }}
+			scrollWheelZoom={false}
+			dragging={width > 767}
+			{...rest}
+		>
+			<TileLayer
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			{children}
+		</MapContainer>
+	);
 };
 
 export default Map;
