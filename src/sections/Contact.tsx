5b196ee915f05c8a2cc127641ef60081ebc87b92
Erdei Gábor <erdei.gabor2002@gmail.com>
2022-10-21 21:14:06 +0200

    added small text at bottom

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Group, Title, Button, Box, createStyles } from "@mantine/core";
+import { Group, Title, Button, Box, createStyles, Text } from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
   root: {
@@ -24,10 +24,11 @@ export default function Contact() {
         sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}` })}
         weight={900}
         align="center"
+        mb='.5em'
       >
         Kérdésed van?
       </Title>
-
+      <Text align="center" weight={700}>Eseményt jelentenél be? Ötleted van? Írj nekünk!</Text>
       <Group position="center" mt="xl">
         <Link href="mailto:info@forrodrot.com">
           <Button size="xl">Írj nekünk!</Button>
